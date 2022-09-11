96a0e749aec54229d7418755c0859de9dde305b0
yeosiowvic <victoryeo1974@gmail.com>
2022-09-11 17:47:00 +0800

    connect web3j to main function

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import java.util.concurrent.ExecutionException;
 
 @EnableJpaRepositories("com.example.demo.persistence.repo") 
 @EntityScan("com.example.demo.persistence.model")
@@ -12,6 +16,21 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+
+    Web3j web3j = Web3j.build(new HttpService(
+      "https://long-orbital-orb.ethereum-goerli.discover.quiknode.pro/f1cbbcf0a86270f3293d0c177ca7c407198f1d74/")); 
+    //EthBlockNumber result = new EthBlockNumber();
+    try {
+      EthBlockNumber result = web3j.ethBlockNumber().sendAsync().get();
+      System.out.println(" The Block Number is: " + result.getBlockNumber().toString());
+    } catch (InterruptedException e) {
+      System.out.println(e);
+    } catch (ExecutionException e) {
+      System.out.println(e);
+    }
+
+    web3j.shutdown();
 	}
 
 }
